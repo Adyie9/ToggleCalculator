c337998ea23392a7ad55da74be06df555d2c1237
Adyie9 <114578740+Adyie9@users.noreply.github.com>
2023-09-13 13:58:55 +0530

    Update index.js

--- toggleCalculator/index.js
+++ toggleCalculator/index.js
@@ -15,34 +15,36 @@ toggle.addEventListener('click', function() {
         cont.style.transition = '1s';
     }
 });
+let display = document.getElementById('display-box');
+let buttons = document.querySelectorAll('.btn'); // Correct the selector
 
-let display=document.getElementById('display-box');
-let buttons=document.querySelectorAll('btn');
+let string = '';
 
-let string="";
-
-let arr = document.querySelectorAll('button');
-arr.forEach(button => {
-    button.addEventListener('click',(e)=>{
-        if(e.target.innerHTML == "="){
-            string= eval(string);
-            display.value=string;
-        }
-        else if(e.target.innerHTML=="AC"){
-            string="";
-            display.value=string;
-        }
-        else if(e.target.innerHTML=="DEL"){
-            string=string.substring(0,string.length-1);
-            display.value=string;
-        }
-        else if(e.target.innerHTML=="%"){
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML === '=') {
+            try {
+                string = eval(string);
+                if (isNaN(string) || !isFinite(string)) {
+                    throw new Error('Invalid Input');
+                }
+                display.value = string;
+            } catch (error) {
+                display.value = 'Syntax Error';
+                string = '';
+            }
+        } else if (e.target.innerHTML === 'AC') {
+            string = '';
+            display.value = string;
+        } else if (e.target.innerHTML === 'DEL') {
+            string = string.substring(0, string.length - 1);
+            display.value = string;
+        } else if (e.target.innerHTML === '%') {
             string = (eval(string) / 100).toString();
             display.value = string;
-        }
-        else {
+        } else {
             string += e.target.innerHTML;
-            display.value=string;
+            display.value = string;
         }
     })
 });
